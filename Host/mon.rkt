fe1a150fa2d88fafc3a055b7cb1a89d82a5ae71a
jzhs <jonstenerson@comcast.net>
2021-02-03 11:50:26 -0700

    Lib load for host

--- Host/mon.rkt
+++ Host/mon.rkt
@@ -1,6 +1,7 @@
 #lang racket
+(provide (all-defined-out))
 
-(require "../host/tether.rkt")
+(require "tether.rkt")
 
 (define (load-file fn adr)
   (with-input-from-file fn #:mode 'binary
@@ -18,4 +19,37 @@
 ;; example: (~r (fetch-word #x100) #:base 16)
 ;;   Should display start of dtb: d00dfeed
 
+
+(define (store-code adr bstr)
+  (for ([i (bytes-length bstr)])
+    (Store adr (bytes-ref bstr i))))
+
+;; example: (store-code #x90000 (bytes #x00 #x00 #x38 #xd5 #xc0 #x03 #x5f #xd6))
+;; puts the bytes at the address.
+
+
+
+(define (read-syms p) 
+  (define (read-syms/acc lis)
+    (define data (read p))
+    (if (not (eof-object? data))
+        (read-syms/acc (cons data lis))
+        (reverse lis) ) )
+  (read-syms/acc '()))
+
+
+        
+(define-syntax-rule (load-lib path addr syms)
+  (begin
+    (load-file (string-append path ".bin") addr)
+    (define s* (read-syms (open-input-file "../extns/stuff.sym")))
+    (define syms (map (λ (x) (list (car x) (+ addr (cadr x)))) s*))))
+
+
+
+(open-serial "/dev/ttyUSB1" 115200)
+(load-lib "../extns/stuff" #x90000 syms)
+
+(define (call/sym id)
+  (Call (cadr (assoc id syms))))
 
