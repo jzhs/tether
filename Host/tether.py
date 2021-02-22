b7ee8ef932974b85c719ed25b116efce6ea8fdc7
jzhs <jonstenerson@comcast.net>
2021-02-21 21:33:33 -0700

    Update tether.py

--- Host/tether.py
+++ Host/tether.py
@@ -1,24 +1,25 @@
 import serial
 import binascii
 
+
 ser = serial.Serial('/dev/ttyUSB1', 115200)
 
 def Load(adr):
     ser.write(b'\01')
-    bins = binascii.unhexlify(adr)
-    ser.write(bins)
-    out = binascii.hexlify(ser.read())
-    print(out)
-
+    byts = adr.to_bytes(4, 'big')
+    ser.write(byts)
+    ser.flush()
+    return ser.read()
 
 def Store(adr, val):
     ser.write(b'\02')
-    bins = binascii.unhexlify(adr)
-    ser.write(bins)
-    bins = binascii.unhexlify(val)
-    ser.write(bins)
+    byts = adr.to_bytes(4, 'big')
+    ser.write(byts)
+    ser.write(val)
+    ser.flush()
     
 def Call(addr):
     ser.write(b'\03')
-    bins = binascii.unhexlify(adr)
-    ser.write(bins)
+    byts = adr.to_bytes(4, 'big')
+    ser.write(byts)
+    ser.flush()
